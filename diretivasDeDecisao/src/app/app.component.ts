43649f264d82d782c3217af3f26c595b8653407d
Márcio César <marcinho060498@gmail.com>
2024-05-03 16:28:49 -0300

    Diretivas de Decisão

---
+++ diretivasDeDecisao/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = true;
+
+  constructor(){
+     
+    // Diretivas de Decisão
+
+    // QUANDO USAMOS:
+
+    // ngIf usando complemento else, usando para exibir ou ocultar elementos tomado por decisões lógicas.
+
+    // ngSwitch 
+  }
+}
